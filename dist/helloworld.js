f751e842f8f4c51afa58518241678b354a9ac6c8
nischal <nischalkshaj5@gmail.com>
2024-01-23 23:42:39 +0530

    new practicals added

--- dist/helloworld.js
+++ dist/helloworld.js
@@ -1,18 +1,18 @@
 "use strict";
 let user = {
-  name: "Nischal",
-  age: 21,
+    name: "Nischal",
+    age: 21,
 };
 console.log(user.name);
 user.name = "Vishnu";
 console.log(user.name);
 console.log("Hello world");
 let employee = {
-  id: 1,
-  name: "Nischal",
-  retire: (date) => {
-    console.log(date);
-  },
+    id: 1,
+    name: "Nischal",
+    retire: (date) => {
+        console.log(date);
+    },
 };
 employee.retire(new Date());
 console.log(employee.name);
--- dist/typeAliasAndOtherTypes.js
+++ dist/typeAliasAndOtherTypes.js
@@ -1,44 +1,46 @@
 "use strict";
 // Alias
 let employees = {
-  id: 1,
-  name: "Nschal",
-  retire: (date) => {
-    console.log(date);
-  },
+    id: 1,
+    name: "Nschal",
+    retire: (date) => {
+        console.log(date);
+    },
 };
 employees.retire(new Date());
 console.log(employees.name);
 // Union types
 function kgToLbs(weight) {
-  if (typeof weight === "number") {
-    return weight * 2.2;
-  } else {
-    return parseInt(weight) * 2.2;
-  }
+    if (typeof weight === "number") {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
 }
 const result = kgToLbs(10);
 const res = kgToLbs("10");
 console.log(res, result);
 let textBox = {
-  drag: () => {
-    console.log("dragging....");
-  },
-  resize: () => {
-    console.log("resizing...");
-  },
-  textSize: 16,
-  backgroundColor: "Crimson",
+    drag: () => {
+        console.log("dragging....");
+    },
+    resize: () => {
+        console.log("resizing...");
+    },
+    textSize: 16,
+    backgroundColor: "Crimson",
 };
 console.log(textBox.textSize);
 console.log(textBox.backgroundColor);
 // Nullable types
 function greet(name) {
-  if (name) {
-    console.log(name.toUpperCase());
-  } else {
-    console.log("No value passed");
-  }
+    if (name) {
+        console.log(name.toUpperCase());
+    }
+    else {
+        console.log("No value passed");
+    }
 }
 greet("nischal");
 greet(null);
